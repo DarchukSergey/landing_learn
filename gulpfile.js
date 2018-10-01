57ff6b38ef79703b87b82d6297b3b8d4093a5ea9
DarchukSergey <DarchukSergey@gmail.com>
2018-10-01 17:20:22 +0300

    first intialization

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp 			=	require('gulp'),
+	  browserSync 	= 	require('browser-sync').create(),
+	  pug 		    =	require('gulp-pug');
+
+	  gulp.task('server',function(){
+	  	browserSync.init({
+	  		server: {
+	  			port:9000,
+	  			baseDir:"build"
+	  		}
+	  	});
+
+	  
+
+	  gulp.watch('build/**/*').on('change', browserSync.reload);
+	  });
+
+	gulp.task('templates:compile', function buildHTML() {
+  		return gulp.src('source/template/index.pug')
+  		.pipe(pug({
+   			 pretty:true
+ 		 }))
+  		.pipe(gulp.dest('build'))
+		});
